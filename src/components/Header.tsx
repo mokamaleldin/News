7f0bb5951ddb1f925fc5b58b542416d116497e35
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-08-26 12:48:22 +0300

    chore: Update Header component with GitHub Clone and home page links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,15 @@ const Header = () => {
     return (
         <div className="flex justify-between px-16 py-5 mb-10 bg-mkee2 font-bold text-main">
             <div>Mkee News</div>
-            <a href="https://github.com/mokamaleldin/News" className="font-medium">
-                GitHub Clone
-            </a>
+            <div className="flex gap-5 font-medium">
+                <a href="https://github.com/mokamaleldin/News">
+                    GitHub Clone
+                </a>
+                <a href="/">
+                    home page
+                </a>
+            </div>
+
         </div>
     )
 }

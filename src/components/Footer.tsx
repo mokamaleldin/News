77ac17fa06f30d337a75f20a36cad03126271574
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-08-26 12:46:44 +0300

    chore: Update Footer component with social media links and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,23 @@
 const Footer = () => {
     return (
-        <footer>
-            <nav>
+        <footer className="flex justify-between items-center px-16 py-5 mt-10 bg-mkee2 font-bold text-main">
 
+            <nav className="font-bold text-xl">
+                Mkee News 
             </nav>
-            <nav>
-                
+            <nav className="flex justify-center items-center gap-6">
+                <a href="https://www.instagram.com/mokamaleldin_">
+                    <img src="./instagram.png" alt="instagram" width={40} height={40} />
+                </a>
+                <a href="https://www.linkedin.com/in/mokamaleldin">
+                    <img src="./linkedin.png" alt="instagram" width={40} height={40} />
+                </a>
+                <a href="https://www.tiktok.com/@mokamaleldin">
+                    <img src="./tiktok.png" alt="instagram" width={40} height={40} />
+                </a>
+                <a href="https://www.youtube.com/@mokamaleldin">
+                    <img src="./youtube.png" alt="instagram" width={40} height={40} />
+                </a>
             </nav>
         </footer>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import Header from './components/Header.tsx'
+import Footer from './components/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
     <App />
+    <Footer />
   </StrictMode>,
 )
